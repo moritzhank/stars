d8bcfb4867117dc7e38876f47e4e2a6e587534ea
moritzhank <moritz.haneke@tu-dortmund.de>
2024-08-28 20:46:59 +0200

    Spotless apply

--- stars-logic-kcmftbl-smtModelChecker/src/main/kotlin/tools/aqua/stars/logic/kcmftbl/smtModelChecker/ReducedSyntaxConversion.kt
+++ stars-logic-kcmftbl-smtModelChecker/src/main/kotlin/tools/aqua/stars/logic/kcmftbl/smtModelChecker/ReducedSyntaxConversion.kt
@@ -17,7 +17,7 @@
 
 package tools.aqua.stars.logic.kcmftbl.smtModelChecker
 
-/** Create deep copy of Formula*/
+/** Create deep copy of Formula */
 /*
 fun copyF(f: Formula): Formula {
   return when (f) {
